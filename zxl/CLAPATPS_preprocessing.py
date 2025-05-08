8b7da28b17d3cb924c3b4e344433a382f553d695
sileme <123054139+sileme@users.noreply.github.com>
2025-05-08 20:55:00 +0800

    Update CLAPATPS_preprocessing.py

--- zxl/CLAPATPS_preprocessing.py
+++ zxl/CLAPATPS_preprocessing.py
@@ -14,24 +14,28 @@ slope = 1
 intercept = -1024.0
 
 # 方法2：手动指定（已知参数时）
-hu_data = data * slope + intercept
+#hu_data = data * slope + intercept
+
 
 # 查看图像数据的形状
 print("Image shape:", image_data.shape)
 # 选择一个切片进行显示
 # 假设图像数据是三维的 (depth, height, width)，我们选择中间的深度切片
 slice_index = image_data.shape[2] // 2  # 选择中间切片
-selected_slice = image_data[:, :, 16]
+selected_slice = image_data[:, :, 20]
 
+print(selected_slice)
 # 显示图像
-plt.figure(figsize=(8, 8))
-plt.imshow(selected_slice, cmap='gray')  # 使用灰度图显示
-plt.title(f'Slice {slice_index}')
-plt.axis('off')  # 关闭坐标轴
+plt.figure(figsize=(10, 8))
+ax = plt.subplot(1, 2, 1)  # 创建一个子图，用于显示灰度图
+im = ax.imshow(selected_slice, cmap='gray')  # 使用灰度图显示
+ax.set_title(f'Slice {slice_index}')
+ax.axis('off')  # 关闭坐标轴
+
+# 添加颜色条（显示数值范围）
+plt.colorbar(im, ax=ax, orientation='vertical')  # 将颜色条与图像关联
+ax2 = plt.subplot(1, 2, 2)  # 创建另一个子图，用于显示颜色条
+ax2.axis('off')  # 关闭坐标轴
+
+plt.tight_layout()
 plt.show()
-
-window_level = 1024
-window_width = 40
-
-windowed = np.clip((hu_data - window_level + window_width/2) / window_width, 0, 1)
-normalized = (hu_data - np.mean(hu_data)) / np.std(hu_data)
